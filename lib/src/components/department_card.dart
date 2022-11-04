1ee0f6cab7f6916773ef7da87ec1e9b1c53ae776
Andrey Maksimov <andrey@tenderowl.com>
2022-11-05 02:03:01 +0300

    Extract deparment card component

---
+++ lib/src/components/department_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:line_icons/line_icon.dart';
+
+import '../models/department.dart';
+
+class DepartmentCard extends StatelessWidget {
+  final Department department;
+
+  const DepartmentCard(this.department, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.black12,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LineIcon.paintBrush(
+                size: 48,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                department.displayName,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.ibmPlexSans(
+                    fontSize: 18, fontWeight: FontWeight.w400),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/components/departments_view.dart
+++ lib/src/components/departments_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:mart/src/services/collection_api.dart';
 
+import 'department_card.dart';
 import '../models/department.dart';
+import '../services/collection_api.dart';
 
 class DepartmetnsView extends StatefulWidget {
   const DepartmetnsView({super.key});
@@ -28,7 +28,7 @@ class _DepartmetnsViewState extends State<DepartmetnsView> {
               childAspectRatio: 1 / 1.25,
             ),
             itemCount: items.length,
-            padding: EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
             itemBuilder: (context, index) => DepartmentCard(items[index]),
           );
         } else {
@@ -37,39 +37,6 @@ class _DepartmetnsViewState extends State<DepartmetnsView> {
           );
         }
       },
-    );
-  }
-}
-
-class DepartmentCard extends StatelessWidget {
-  final Department department;
-
-  const DepartmentCard(this.department, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.black12,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                department.displayName,
-                textAlign: TextAlign.center,
-                style: GoogleFonts.ibmPlexSans(
-                    fontSize: 18, fontWeight: FontWeight.w400),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
